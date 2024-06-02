b44b509390ab84a7e1a75e39f1968d6fd1c43435
Jongheon Park <48527182+hyphmngo@users.noreply.github.com>
2024-06-01 23:20:30 -0400

    [nvim] Remove esc keymap in favor of ctrl-l

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -1,4 +1,2 @@
-vim.keymap.set('n', '<Esc>', '<Cmd>nohlsearch<CR>',
-               { desc = "Clear search highlights" })
 vim.keymap.set('n', '<Leader>ss', '<Cmd>mks!<CR>',
                { desc = "Save session" })
