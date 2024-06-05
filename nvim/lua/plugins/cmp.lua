852ee0a91c33cad4af1847e0f496206cdd74d29c
Jongheon Park <48527182+hyphmngo@users.noreply.github.com>
2024-06-05 11:30:45 -0400

    [nvim] Add mappings for snippet

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -30,12 +30,31 @@ return {
           end
         },
         mapping = cmp.mapping.preset.insert({
-          ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-          ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if vim.snippet.active({ direction = 1}) then
+              vim.snippet.jump(1)
+            else
+              fallback()
+            end
+          end, {'i', 's'}),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if vim.snippet.active({ direction = -1 }) then
+              vim.snippet.jump(-1)
+            else
+              fallback()
+            end
+          end, {'i', 's'}),
+          ['<C-n>'] = cmp.mapping.select_next_item({
+            behavior = cmp.SelectBehavior.Select
+          }),
+          ['<C-p>'] = cmp.mapping.select_prev_item({
+            behavior = cmp.SelectBehavior.Select
+          }),
           ['<C-u>'] = cmp.mapping.scroll_docs({ delta = 4 }),
           ['<C-d>'] = cmp.mapping.scroll_docs({ delta = -4 }),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<Enter>'] = cmp.mapping.confirm({ select = true })
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
         }),
         snippet = {
           expand = function(args)
