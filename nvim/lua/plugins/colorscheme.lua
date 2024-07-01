9a399a983449698637e898aae133852ab2f1cbec
Jongheon Park <48527182+hyphmngo@users.noreply.github.com>
2024-07-01 09:43:14 -0400

    [nvim] Rename

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      no_italic = true,
+      custom_highlights = function(colors)
+        return {
+          FloatBorder = { bg = colors.mantle },
+          FzfLuaHeaderBind = { fg = colors.surface2 },
+          FzfLuaHeaderText = { fg = colors.surface2 },
+          FzfLuaPathColNr = { fg = colors.text },
+          FzfLuaPathLineNr = { fg = colors.text },
+          FzfLuaBufName = { fg = colors.blue }, -- For FzfLua lines
+          FzfLuaBufNr = { fg = colors.text },
+          FzfLuaBufFlagCur = { fg = colors.red },
+          FzfLuaBufFlagAlt = { fg = colors.green },
+          FzfLuaTabTitle = { fg = colors.text },
+          FzfLuaTabMarker = { fg = colors.red },
+          FzfLuaLiveSym = { fg = colors.green },
+          MiniStatuslineDevinfo = { bg = colors.base },
+          MiniStatuslineFilename = { bg = colors.base },
+          MiniStatuslineFileinfo = { bg = colors.base },
+          MiniStatuslineInactive = { fg = colors.surface2, bg = colors.base }
+        }
+      end
+    },
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+      vim.cmd.colorscheme('catppuccin')
+    end
+  }
+}
