812877afd733e3212c25764781c32031222ee2d3
Jongheon Park <48527182+tinygabagool@users.noreply.github.com>
2024-08-13 22:48:02 -0400

    [nvim] Alphabetically sort opts

--- nvim/.config/nvim/plugin/opts.lua
+++ nvim/.config/nvim/plugin/opts.lua
@@ -1,40 +1,17 @@
 local opt = vim.opt
 
-opt.clipboard = 'unnamedplus' -- TODO: OSC52
-
-opt.mouse = 'a'
-opt.mousescroll = { 'ver:1', 'hor:1' }
-
-opt.number = true
-opt.relativenumber = true
-
-opt.cursorline = true
-opt.cursorlineopt = 'number'
-
-opt.splitright = true
-opt.splitbelow = true
-
--- Softwrap
-opt.linebreak = true
 opt.breakat = ' \t;,!?'
 opt.breakindent = true
 opt.breakindentopt = { 'shift:2', 'sbr' }
 
-opt.ignorecase = true
-opt.smartcase = true
+opt.clipboard = 'unnamedplus' -- TODO: OSC52
 
--- Insert 2 <S>s instead of <Tab> while editing
-opt.expandtab = true -- Insert spaces instead of tabs
-opt.shiftwidth = 2 -- Number of whitespace for (auto)indent
-opt.softtabstop = -1 -- Use shiftwidth's value
-
--- Pop up menu even when only one match, explicit insert & select by user
--- Not sure about preview... does it do anything for cmp?
 opt.completeopt = {'menu', 'menuone', 'noselect' }
-opt.inccommand = 'split'
 
--- Used for CursorHold autocmd
-opt.updatetime = 250
+opt.cursorline = true
+opt.cursorlineopt = 'number'
+
+opt.expandtab = true -- Insert spaces instead of tabs
 
 -- Don't insert comment leader after 'o' or 'O' in insert mode
 vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
@@ -42,3 +19,28 @@ vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
     opt.formatoptions:remove('o')
   end
 })
+
+opt.grepprg = 'rg --vimgrep --smart-case'
+
+opt.ignorecase = true
+
+opt.inccommand = 'split'
+
+opt.linebreak = true -- Softwrap
+
+opt.mouse = 'a'
+opt.mousescroll = { 'ver:1', 'hor:1' }
+
+opt.number = true
+opt.relativenumber = true
+
+opt.shiftwidth = 2 -- Number of whitespace for (auto)indent
+
+opt.smartcase = true
+
+opt.softtabstop = -1 -- Use shiftwidth's value
+
+opt.splitbelow = true
+opt.splitright = true
+
+opt.updatetime = 250 -- Used for CursorHold autocmd
